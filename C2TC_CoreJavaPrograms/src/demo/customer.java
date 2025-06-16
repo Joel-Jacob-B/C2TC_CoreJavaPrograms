4bd50baf107b7e1750da43de66c0ecfff71530d9
Joel-Jacob-B <joeljacob210@gmail.com>
2025-06-16 13:43:39 +0530

    This is my Core Java Programs

---
+++ C2TC_CoreJavaPrograms/src/demo/customer.java
@@ -0,0 +1,38 @@
+package demo;
+
+public class customer {
+
+	private int id;
+	private String name, city;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getCity() {
+		return this.city;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+
+	@Override
+	public String toString() {
+		return "customer [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+	
+	
+}
