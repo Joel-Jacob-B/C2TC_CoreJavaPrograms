e8e350c186299ba34c1959e4a9efee951eed26c6
Joel-Jacob-B <joeljacob210@gmail.com>
2025-07-01 09:55:21 +0530

    This is my Core Java Programs

--- C2TC_CoreJavaPrograms/src/com/tnsif/day9/WrapperClassDemo.java
+++ C2TC_CoreJavaPrograms/src/com/tnsif/day9/WrapperClassDemo.java
@@ -4,7 +4,7 @@ package com.tnsif.day9;
 public class WrapperClassDemo {
 	public static void main(String[] args) {
 		// Unboxing
-		Integer i = new Integer(10);
+		Integer i = 10;
 		System.out.println(i);
 		int b = i.intValue();
 		System.out.println(b);
